a78f243c0823aaeffc6379a9dc98cbe000b30803
ArslanYM <arsalanyaqmalik@gmail.com>
2023-09-14 19:51:04 +0530

    enabled cors on the api

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
 const PORT = 3000;
 const issueRouter = require('./routes/issues');
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", issueRouter);
